ae7c7cb884f9b94f5ae8d3e26466bc9a106305d9
rez047 <113913401+rez047@users.noreply.github.com>
2023-06-16 23:57:21 +0300

    Update views.py

--- school_management_app/views.py
+++ school_management_app/views.py
@@ -22,101 +22,106 @@ from school_management_app.forms import LineItemFormset, InvoiceForm
 import pdfkit
 
 def ShowLoginPage(request):
-    return render(request,"login_page.html")
+    return render(request, "login_page.html")
 
 def doLogin(request):
-    if request.method!="POST":
+    if request.method != "POST":
         return HttpResponse("<h2>Method Not Allowed</h2>")
     else:
-        # captcha_token=request.POST.get("g-recaptcha-response")
-        # cap_url="https://www.google.com/recaptcha/api/siteverify"
-        # cap_secret="6LeWtqUZAAAAANlv3se4uw5WAg-p0X61CJjHPxKT"
-        # cap_data={"secret":cap_secret,"response":captcha_token}
-        # cap_server_response=requests.post(url=cap_url,data=cap_data)
-        # cap_json=json.loads(cap_server_response.text)
+        username_or_email = request.POST.get("username_or_email")
+        password = request.POST.get("password")
 
-        # if cap_json['success']==False:
-        #     messages.error(request,"Invalid Captcha Try Again")
-        #     return HttpResponseRedirect("/")
+        # Check if the input is a valid email
+        if '@' in username_or_email:
+            user = EmailBackEnd.authenticate(request, username=username_or_email, password=password)
+        else:
+            user = authenticate(request, username=username_or_email, password=password)
 
-        user=EmailBackEnd.authenticate(request,username=request.POST.get("email"),password=request.POST.get("password"))
-        if user!=None:
-            login(request,user)
-            if user.user_type=="1":
+        if user is not None:
+            login(request, user)
+            if user.user_type == "1":
                 return HttpResponseRedirect('/admin_home')
-            elif user.user_type=="2":
+            elif user.user_type == "2":
                 return HttpResponseRedirect(reverse("staff_home"))
             else:
                 return HttpResponseRedirect(reverse("student_home"))
         else:
-            messages.error(request,"Invalid Login Details")
+            messages.error(request, "Invalid Login Details")
             return HttpResponseRedirect("/")
 
 
 def GetUserDetails(request):
-    if request.user!=None:
-        return HttpResponse("User : "+request.user.email+" usertype : "+str(request.user.user_type))
+    if request.user is not None:
+        return HttpResponse("User: " + request.user.email + " usertype: " + str(request.user.user_type))
     else:
         return HttpResponse("Please Login First")
+
 
 def logout_user(request):
     logout(request)
     return HttpResponseRedirect("/")
 
+
 def Testurl(request):
     return HttpResponse("Ok")
 
+
 def signup_admin(request):
-    return render(request,"signup_admin_page.html")
+    return render(request, "signup_admin_page.html")
+
 
 def signup_student(request):
-    courses=Courses.objects.all()
-    session_years=SessionYearModel.object.all()
-    return render(request,"signup_student_page.html",{"courses":courses,"session_years":session_years})
+    courses = Courses.objects.all()
+    session_years = SessionYearModel.objects.all()
+    return render(request, "signup_student_page.html", {"courses": courses, "session_years": session_years})
+
 
 def signup_staff(request):
-    return render(request,"signup_staff_page.html")
+    return render(request, "signup_staff_page.html")
+
 
 def do_admin_signup(request):
-    username=request.POST.get("username")
-    email=request.POST.get("email")
-    password=request.POST.get("password")
-    default_profile_pic="/media/default.png"
-    if request.FILES.get('profile_pic',False):
-        profile_pic=request.FILES['profile_pic']
-        fs=FileSystemStorage()
-        filename=fs.save(profile_pic.name,profile_pic)
-        profile_pic_url=fs.url(filename)
+    username = request.POST.get("username")
+    email = request.POST.get("email")
+    password = request.POST.get("password")
+    default_profile_pic = "/media/default.png"
+    if request.FILES.get('profile_pic', False):
+        profile_pic = request.FILES['profile_pic']
+        fs = FileSystemStorage()
+        filename = fs.save(profile_pic.name, profile_pic)
+        profile_pic_url = fs.url(filename)
     else:
-        profile_pic_url=None
+        profile_pic_url = None
     try:
-        user=CustomUser.objects.create_user(username=username,password=password,email=email,user_type=1)
-        if profile_pic_url!=None:
-            user.adminhod.profile_pic=profile_pic_url
+        user = CustomUser.objects.create_user(username=username, password=password, email=email, user_type=1)
+        if profile_pic_url is not None:
+            user.adminhod.profile_pic = profile_pic_url
         else:
-            user.adminhod.profile_pic=default_profile_pic
+            user.adminhod.profile_pic = default_profile_pic
         user.save()
-        messages.success(request,"Successfully Created Admin")
+        messages.success(request, "Successfully Created Admin")
         return HttpResponseRedirect(reverse("show_login"))
     except:
-        messages.error(request,"Failed to Create Admin")
+        messages.error(request, "Failed to Create Admin")
         return HttpResponseRedirect(reverse("show_login"))
+
 
 def do_staff_signup(request):
-    username=request.POST.get("username")
-    email=request.POST.get("email")
-    password=request.POST.get("password")
-    address=request.POST.get("address")
+    username = request.POST.get("username")
+    email = request.POST.get("email")
+    password = request.POST.get("password")
+    address = request.POST.get("address")
 
     try:
-        user=CustomUser.objects.create_user(username=username,password=password,email=email,user_type=2)
-        user.staffs.address=address
+        user = CustomUser.objects.create_user(username=username, password=password, email=email, user_type=2)
+        user.staffs.address = address
         user.save()
-        messages.success(request,"Successfully Created Staff")
+        messages.success(request, "Successfully Created Staff")
         return HttpResponseRedirect(reverse("show_login"))
     except:
-        messages.error(request,"Failed to Create Staff")
+        messages.error(request, "Failed to Create Staff")
         return HttpResponseRedirect(reverse("show_login"))
+
 
 def do_signup_student(request):
     first_name = request.POST.get("first_name")
@@ -128,14 +133,14 @@ def do_signup_student(request):
     session_year_id = request.POST.get("session_year")
     course_id = request.POST.get("course")
     sex = request.POST.get("sex")
-    default_profile_pic="/media/default.png"
-    if request.FILES.get('profile_pic',False):
-        profile_pic=request.FILES['profile_pic']
-        fs=FileSystemStorage()
-        filename=fs.save(profile_pic.name,profile_pic)
-        profile_pic_url=fs.url(filename)
+    default_profile_pic = "/media/default.png"
+    if request.FILES.get('profile_pic', False):
+        profile_pic = request.FILES['profile_pic']
+        fs = FileSystemStorage()
+        filename = fs.save(profile_pic.name, profile_pic)
+        profile_pic_url = fs.url(filename)
     else:
-        profile_pic_url=None
+        profile_pic_url = None
     try:
         user = CustomUser.objects.create_user(username=username, password=password, email=email, last_name=last_name, first_name=first_name, user_type=3)
         user.students.address = address
@@ -144,7 +149,7 @@ def do_signup_student(request):
         session_year = SessionYearModel.object.get(id=session_year_id)
         user.students.session_year_id = session_year
         user.students.gender = sex
-        if profile_pic_url!=None:
+        if profile_pic_url is not None:
             user.students.profile_pic = profile_pic_url
         else:
             user.students.profile_pic = default_profile_pic
@@ -154,13 +159,14 @@ def do_signup_student(request):
     except:
         messages.error(request, "Failed to Add Student")
         return HttpResponseRedirect(reverse("show_login"))
-        
+
+
 def bulk_upload(request):
     if request.method == 'POST':
         form = BulkInvoiceUploadForm(request.POST, request.FILES)
         if form.is_valid():
             file = request.FILES['file']
-            
+
             # Process the file and save the invoices to the database
             import pandas as pd
 
@@ -206,5 +212,3 @@ def bulk_upload(request):
 
 def success_page(request):
     return render(request, 'hod_template/success_page.html')
-
-
