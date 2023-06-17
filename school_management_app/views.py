f66d7aac862845d98da63cb16db65931d2a4305e
rez047 <113913401+rez047@users.noreply.github.com>
2023-06-17 13:20:57 +0300

    Update views.py

--- school_management_app/views.py
+++ school_management_app/views.py
@@ -31,20 +31,31 @@ def doLogin(request):
     if request.method != "POST":
         return HttpResponse("<h2>Method Not Allowed</h2>")
     else:
+        # captcha_token=request.POST.get("g-recaptcha-response")
+        # cap_url="https://www.google.com/recaptcha/api/siteverify"
+        # cap_secret="6LeWtqUZAAAAANlv3se4uw5WAg-p0X61CJjHPxKT"
+        # cap_data={"secret":cap_secret,"response":captcha_token}
+        # cap_server_response=requests.post(url=cap_url,data=cap_data)
+        # cap_json=json.loads(cap_server_response.text)
+
+        # if cap_json['success']==False:
+        #     messages.error(request,"Invalid Captcha Try Again")
+        #     return HttpResponseRedirect("/")
+
         username = request.POST.get("username")
         password = request.POST.get("password")
+        user = None
 
+        # Check if the username contains "@" symbol
         if "@" in username:
-            user = EmailBackEnd.authenticate(
-                request, username=username, password=password
-            )
+            user = EmailBackEnd().authenticate(request, username=username, password=password)
         else:
             user = authenticate(request, username=username, password=password)
 
         if user is not None:
             login(request, user)
             if user.user_type == "1":
-                return HttpResponseRedirect("/admin_home")
+                return HttpResponseRedirect('/admin_home')
             elif user.user_type == "2":
                 return HttpResponseRedirect(reverse("staff_home"))
             else:
