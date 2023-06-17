5745fef4d87212d5d38d2a192b1138559a4a7fc4
rez047 <113913401+rez047@users.noreply.github.com>
2023-06-17 16:09:39 +0300

    Update views.py

--- school_management_app/views.py
+++ school_management_app/views.py
@@ -41,10 +41,7 @@ def doLogin(request):
         if "@" in email_or_username:
             user = backend.authenticate(request, username=email, password=password)
         else:
-            # Check if the input is a username
-            username = backend.get_username(email_or_username)
-            if username:
-                user = backend.authenticate(request, username=username, password=password)
+            user = backend.authenticate(request, username=username, password=password)
 
         if user is not None:
             login(request, user)
