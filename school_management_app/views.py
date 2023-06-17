65ac5a82e967b2424ceb02f7fa32ff988aaa5ab1
rez047 <113913401+rez047@users.noreply.github.com>
2023-06-17 15:13:02 +0300

    Update views.py

--- school_management_app/views.py
+++ school_management_app/views.py
@@ -32,17 +32,6 @@ def doLogin(request):
     if request.method != "POST":
         return HttpResponse("<h2>Method Not Allowed</h2>")
     else:
-        # captcha_token=request.POST.get("g-recaptcha-response")
-        # cap_url="https://www.google.com/recaptcha/api/siteverify"
-        # cap_secret="6LeWtqUZAAAAANlv3se4uw5WAg-p0X61CJjHPxKT"
-        # cap_data={"secret":cap_secret,"response":captcha_token}
-        # cap_server_response=requests.post(url=cap_url,data=cap_data)
-        # cap_json=json.loads(cap_server_response.text)
-
-        # if cap_json['success']==False:
-        #     messages.error(request,"Invalid Captcha Try Again")
-        #     return HttpResponseRedirect("/")
-
         email = request.POST.get("email")
         username = request.POST.get("username")
         password = request.POST.get("password")
@@ -54,10 +43,22 @@ def doLogin(request):
             if username:
                 user = EmailBackEnd().authenticate(request, username=username, password=password)
         else:
-
             username = EmailBackEnd().get_username(username)
             if username:
                 user = EmailBackEnd().authenticate(request, username=username, password=password)
+
+        if user is not None:
+            login(request, user)
+            if user.user_type == "1":
+                return HttpResponseRedirect('/admin_home')
+            elif user.user_type == "2":
+                return HttpResponseRedirect(reverse("staff_home"))
+            else:
+                return HttpResponseRedirect(reverse("student_home"))
+        else:
+            messages.error(request, "Invalid Login Details")
+            return HttpResponseRedirect("/")
+
             
 
         if user is not None:
