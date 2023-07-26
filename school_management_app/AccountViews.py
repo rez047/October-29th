08cf186d0a659c2dd95b2d02771bac851b251faa
rez047 <113913401+rez047@users.noreply.github.com>
2023-07-26 22:31:04 +0300

    Update AccountViews.py

--- school_management_app/AccountViews.py
+++ school_management_app/AccountViews.py
@@ -23,9 +23,10 @@ import io
 from xhtml2pdf import pisa
 from django.template import Context
 from django.db import transaction
+from datetime import datetime
 
 from school_management_app.forms import  InvoiceForm,LineItemFormset
-from school_management_app.models import LeaveReportAccount, Accounts, FeedBackAccounts, CustomUser, NotificationAccounts, ANews, AComment, Invoice, LineItem, FinancialRecord, Students, DefaultSettings
+from school_management_app.models import LeaveReportAccount, Accounts, FeedBackAccounts, CustomUser, NotificationAccounts, ANews, AComment, Invoice, LineItem, FinancialRecord, Students, DefaultSettings, SessionYearModel, Courses
 
 def account_home(request):
     student_count7=Students.objects.all().count()
@@ -233,67 +234,110 @@ def account_all_notification(request):
     return render(request,"account_template/all_notification.html",{"notifications":notifications,"account":account})
     
 
-
 def account_add_financial_record(request):
     user = CustomUser.objects.get(id=request.user.id)
     records = FinancialRecord.objects.all()
-    students = CustomUser.objects.filter(user_type=3)
+    session_years=SessionYearModel.object.all()
+    courses = Courses.objects.all()
+    
 
     if request.method == "POST":
-        student_id = request.POST.get("student_id")
+        student_admin_id= request.POST.get("student_list")
+        student_user = CustomUser.objects.get(id=student_admin_id)
+        student_obj = Students.objects.get(admin=student_admin_id)
+        student = Students.objects.get(admin=student_user)
+        course_id = request.POST.get("course")
+        course_obj = Courses.objects.get(id=course_id)
         date = request.POST.get("date")
         fee_type = request.POST.get("fee_type")
-        amount_paid = Decimal(request.POST.get("amount_paid"))  # Convert to Decimal
+        session_year_id = request.POST.get("session_year")
+
+
+
+        # Validate the date format before creating the record
+        if date:
+            try:
+                # Date format expected: "YYYY-MM-DD"
+                date_obj = datetime.strptime(date, "%Y-%m-%d")
+            except ValueError:
+                error_message = "Failed to add financial record. Invalid date format."
+                return HttpResponse(error_message)
+        else:
+            # Handle the case when the date is not provided
+            error_message = "Failed to add financial record. Date not provided."
+            return HttpResponse(error_message)
+
+
+        # Get the value of "amount_paid" from the request.POST dictionary
+        amount_paid_str = request.POST.get("amount_paid")
+
+        # Check if the value is not empty or None before converting to Decimal
+        if amount_paid_str:
+            amount_paid = Decimal(amount_paid_str)
+        else:
+            amount_paid = Decimal("0.00")  # Default value if amount_paid is empty
 
         try:
-            student_user = CustomUser.objects.get(id=student_id)
+            student_user = CustomUser.objects.get(id=student_admin_id)
+            student_obj = Students.objects.get(admin=student_user)
             student = Students.objects.get(admin=student_user)
+            course = Courses.objects.get(id=course_id)
+            session_year=SessionYearModel.object.get(id=session_year_id)
 
-            # Retrieve the latest financial record for the student, regardless of the fee type
-            latest_record = FinancialRecord.objects.filter(student=student).latest('created_at')
-            fee_balance = latest_record.new_balance
+            # Retrieve the latest financial record for the student with the same fee_type (if exists)
+            try:
+                latest_record_same_fee = FinancialRecord.objects.filter(student=student, fee_type=fee_type).latest('created_at')
+                latest_recor = FinancialRecord.objects.filter(student=student).latest('created_at')
+                fee_balance = latest_recor.new_balance
+            except FinancialRecord.DoesNotExist:
+                # If no previous record exists for the student with the same fee_type, set fee_balance to the default fee
+                default_settings = DefaultSettings.objects.first()
+                if default_settings is not None:
+                    default_fees = {
+                        'lunch': default_settings.lunch_fee,
+                        'transport': default_settings.transport_fee,
+                        'tuition': default_settings.tuition_fee,
+                        'transport and lunch': default_settings.transport_and_lunch_fee,
+                        'tuition and transport': default_settings.tuition_and_transport_fee,
+                        'tuition and lunch': default_settings.tuition_and_lunch_fee,
+                        'tuition, lunch and transport': default_settings.tuition_lunch_transport_fee,
+                        'other': default_settings.other_fee,
+                    }
+                    default_time = default_settings.default_time.strftime('%Y-%m-%d')
+                else:
+                    # Create new default settings with default values
+                    default_settings = DefaultSettings.objects.create(
+                        lunch_fee=1000,
+                        transport_fee=2000,
+                        tuition_fee=100000,
+                        transport_and_lunch_fee=0,
+                        tuition_and_transport_fee=0,
+                        tuition_and_lunch_fee=0,
+                        tuition_lunch_transport_fee=0,
+                        other_fee=0
+                    )
+                    default_fees = {
+                        'lunch': default_settings.lunch_fee,
+                        'transport': default_settings.transport_fee,
+                        'tuition': default_settings.tuition_fee,
+                        'transport and lunch': default_settings.transport_and_lunch_fee,
+                        'tuition and transport': default_settings.tuition_and_transport_fee,
+                        'tuition and lunch': default_settings.tuition_and_lunch_fee,
+                        'tuition, lunch and transport': default_settings.tuition_lunch_transport_fee,
+                        'other': default_settings.other_fee,
+                    }
+                    default_time = default_settings.default_time.strftime('%Y-%m-%d')
+
+                latest_record = FinancialRecord.objects.filter(student=student).latest('created_at')
+                fee_balance = default_fees.get(fee_type, 0)+latest_record.new_balance
 
             # Calculate the new balance by subtracting the amount paid from the fee_balance
-            new_balance = fee_balance - amount_paid
-
-            # Retrieve the latest new_balance regardless of the fee type
-            latest_new_balance = FinancialRecord.objects.filter(student=student).latest('created_at').new_balance
-
-            # Retrieve the default fees and date from the DefaultSettings model
-            try:
-                default_settings = DefaultSettings.objects.first()
-                default_fees = {
-                    'lunch': default_settings.lunch_fee,
-                    'transport': default_settings.transport_fee,
-                    'tuition': default_settings.tuition_fee,
-                    'transport and lunch': default_settings.transport_and_lunch_fee,
-                    'tuition and transport': default_settings.tuition_and_transport_fee,
-                    'tuition and lunch': default_settings.tuition_and_lunch_fee,
-                    'tuition, lunch and transport': default_settings.tuition_lunch_transport_fee,
-                    'other': default_settings.other_fee,
-                }
-                default_time = default_settings.default_time.strftime('%Y-%m-%d')
-            except DefaultSettings.DoesNotExist:
-                default_fees = {
-                    'lunch': 1000,
-                    'transport': 2000,
-                    'tuition': 100000,
-                    'transport and lunch': 0,
-                    'tuition and transport': 0,
-                    'tuition and lunch': 0,
-                    'tuition, lunch and transport': 0,
-                    'other': 0,
-                }
-                default_time = "2023-06-16 15:46:00"  # Set the default date as per your requirement
-
-            # Add the default fee for the selected fee type and the latest new_balance
-            fee_balance = default_fees.get(fee_type, 0) + latest_new_balance
             new_balance = fee_balance - amount_paid
 
             # Create the financial record
             record = FinancialRecord.objects.create(
                 student=student,
-                date=date,
+                date=date_obj,  # Use the parsed date_obj instead of the date string
                 fee_type=fee_type,
                 fee_balance=fee_balance,
                 amount_paid=amount_paid,
@@ -303,37 +347,8 @@ def account_add_financial_record(request):
             # Redirect or display a success message
             return HttpResponseRedirect(reverse("account_financial_record_list"))
         except Students.DoesNotExist:
-            error_message = "Failed to add financial record. Student with ID {} not found.".format(student_id)
+            error_message = "Failed to add financial record. Student with ID {} not found.".format(student_admin_id)
             return HttpResponse(error_message)
-        except FinancialRecord.DoesNotExist:
-            # If no previous record exists for the student, set fee_balance to the default fee for the selected fee type
-            default_fees = {
-                'lunch': 1000,
-                'transport': 2000,
-                'tuition': 100000,
-                'transport and lunch': 0,
-                'tuition and transport': 0,
-                'tuition and lunch': 0,
-                'tuition, lunch and transport': 0,
-                'other': 0,
-            }
-            fee_balance = default_fees.get(fee_type, 0)
-
-            # Calculate the new balance by subtracting the amount paid from the fee_balance
-            new_balance = fee_balance - amount_paid
-
-            # Create the financial record
-            record = FinancialRecord.objects.create(
-                student=student,
-                date=date,
-                fee_type=fee_type,
-                fee_balance=fee_balance,
-                amount_paid=amount_paid,
-                new_balance=new_balance
-            )
-
-            # Redirect or display a success message
-            return HttpResponseRedirect(reverse("account_financial_record_list"))
         except Exception as e:
             error_message = "Failed to add financial record. Error: {}".format(str(e))
             return HttpResponse(error_message)
@@ -375,7 +390,8 @@ def account_add_financial_record(request):
                     'tuition, lunch and transport': default_settings.tuition_lunch_transport_fee,
                     'other': default_settings.other_fee,
                 }
-                default_time = default_settings.default_time.strftime('%Y-%m-%d')
+                default_time = default_settings.default_time.strftime('%Y-%m-%d')  # Set the default date as per your requirement
+
         except DefaultSettings.DoesNotExist:
             # Create new default settings with default values
             default_settings = DefaultSettings.objects.create(
@@ -398,9 +414,9 @@ def account_add_financial_record(request):
                 'tuition, lunch and transport': default_settings.tuition_lunch_transport_fee,
                 'other': default_settings.other_fee,
             }
-            default_time = default_settings.default_time.strftime('%Y-%m-%d')# Set the default date as per your requirement
-    
-        return render(request, "account_template/financial_record_add.html", {"students": students, "default_fees": default_fees, "default_time": default_time})
+            default_time = default_settings.default_time.strftime('%Y-%m-%d')  # Set the default date as per your requirement
+
+    return render(request, "account_template/financial_record_add.html", { "session_years":session_years, "courses": courses, "default_fees": default_fees, "default_time": default_time})
 
 def account_default_settings(request):
     # Retrieve the existing default settings if they exist
@@ -550,6 +566,7 @@ def account_generate_pdf(content):
     return pdf
 
 
+
 def account_generate_receipt_pdfs(request, student_id):
     try:
         records = FinancialRecord.objects.filter(student__id=student_id)
@@ -594,4 +611,3 @@ def account_generate_pdfs(content):
     buffer.close()
 
     return pdf
-
