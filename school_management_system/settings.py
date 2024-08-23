ce1540b275658572039891eb91ffe130de8ecf42
rez047 <113913401+rez047@users.noreply.github.com>
2024-08-23 18:30:26 +0300

    Update settings.py

--- school_management_system/settings.py
+++ school_management_system/settings.py
@@ -57,11 +57,11 @@ TEMPLATES = [
                 'django.template.context_processors.request',
                 'django.contrib.auth.context_processors.auth',
                 'django.contrib.messages.context_processors.messages',
-                'django.template.context_processors.messages',
             ],
         },
     },
 ]
+
 
 WSGI_APPLICATION = 'school_management_system.wsgi.application'
 
